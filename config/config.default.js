0981d1e62bf127caae038d724b7bdee3ad4cedcf
孙卫峰 <weifeng.sun@dianrong.com>
2017-08-16 13:51:10 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1502862573589_521';
+
+  // add your config here
+
+  return config;
+};
